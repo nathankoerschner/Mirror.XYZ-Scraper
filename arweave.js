a06d4bf0dcba3e6903020b46155339325a020d75
nathanabram <nathan@nathanabram.com>
2022-01-19 08:54:52 -0500

    fixed fetching

--- arweave.js
+++ arweave.js
@@ -1,43 +1,67 @@
 const { request, gql, GraphQLClient } = require('graphql-request')
 const fs = require('fs');
+const { findSourceMap } = require('module');
+const { resourceLimits } = require('worker_threads');
 const endpoint = 'https://arweave.net/graphql'
 
 
-const requestData = async (startCursor) => {
-    let txs = [];
-    let cursor = null
+const requestData = async (startBlock, step) => {
+    const stopBlock = startBlock + step;
+    let toReturn = [];
+
     let query = gql`
-        {
-            transactions(first:100, after: "${startCursor}", tags: {name: "App-Name", values: "MirrorXYZ"}) {
-                edges {
-                cursor
-                    node {
-                        id
+    {
+        transactions(first:100, block: {min:${startBlock}, max:${stopBlock}}, tags: {name: "App-Name", values: "MirrorXYZ"}) {
+            edges {
+            cursor
+                node {
+                    id
+                    tags{
+                        name
+                        value
                     }
                 }
             }
         }
-        `
+    }
+    `
+
+
     await request(endpoint, query).then( (data) => {
-        for (let i = 0; i < 100; i++){
-            txs.push(data.transactions.edges[i].node.id) // data.transactions.edges
-            
-            if (i == 99) { // if there was a full page of data gathered
-                cursor =  data.transactions.edges[i].cursor;
-            }
+        if (data.transactions.edges){
+            amount = Object.keys(data.transactions.edges).length
+            console.log("found " + amount + " tickets matching the query")
+            toReturn = data.transactions.edges;
+        } else {
+            console.log("No transactions found.");
+            toReturn = [];
         }
     })
-    return { txs, cursor } 
+    return toReturn;
     
 }
 
-// TODO: create a paginator, which fires requestData() once, and then repeats it with the cursor of the last one, until cursor is null.
 
-const loadData = async () => {
-    const finalData = await requestData("")
 
-    let jsonData = JSON.stringify(finalData["txs"]);
-    fs.writeFileSync("data.json", jsonData);
+
+const findAll = async () => {
+    startBlock = 550000; // first Arweave transaction is at 559678
+    endBlock = 750000;// 751000 is where they get dense
+    step = 1000;
+    results = [];
+
+    currentBlock = startBlock
+    while (currentBlock < endBlock) {
+            console.log("\n\n\n Current Block is ", currentBlock);
+            await requestData(currentBlock, step).then((data) => {
+                results = results.concat(data);
+            });
+        currentBlock += step;
+    }
+
+    console.log(results)
+    let jsonData = JSON.stringify(results);
+    fs.writeFileSync(`blocks_${startBlock}_to_${endBlock}.json`, jsonData);
 }
 
-loadData()
+findAll();
