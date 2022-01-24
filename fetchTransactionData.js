21bddf33d7c4dad46ee8881364022266c31605c3
nathanabram <nathan@nathanabram.com>
2022-01-24 13:50:01 -0500

    setup fetching

--- fetchTransactionData.js
+++ fetchTransactionData.js
@@ -1,31 +1,20 @@
-const request = require("request");
 const fs = require ('fs')
+const reqData = require('./request.js')
 
-let id = "FQpnEGufbaR0nrLiOzcKCigrwb1GtEFjZwIEnst7xNw";
 
-const fetchTicketData = (ticketId)=>{
-    let options = {
-        method: 'GET',
-        url: `https://arweave.net/${ticketId}/data`
-       };
-       
-       request(options, function (error, response, body) { 
-         if (error){
-          console.error(error); 
-         }
-         //console.log('Arweave network height is: ' + JSON.parse(body).height);
-         //console.log(body)
-         return(JSON.parse(body))
-       });
+
+var array = fs.readFileSync('mirrorTickets.csv', 'utf-8')
+    .split('\n')
+    .map(line => line.split(","));
+
+const fetchData = async () =>{
+    let promises = array.map( async x => {
+
+    });
+    
+    for await (const val of promises){
+        console.log(val.response);
+    }
 }
 
-
-var output = fs.readFileSync('mirrorTickets.csv', 'utf-8')
-    .split('\n')
-    .map(line => line.split(","))
-    .reduce((id, line) => {
-
-    }, [])
-
-console.log(output)
-//https://www.youtube.com/watch?v=1DMolJ2FrNY&list=PL0zVEGEvSaeEd9hlmCXrk5yUyqUag-n84&index=4 going off of this to try and learn .reduce()
+fetchData()
