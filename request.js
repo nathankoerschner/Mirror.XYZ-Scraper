d797e85bde3bd6bcf2e239bed0f48113ec43e86e
nathanabram <nathan@nathanabram.com>
2022-01-25 10:35:39 -0500

    implimented spaced fetching

--- request.js
+++ request.js
@@ -13,14 +13,12 @@ const reqData = async function(id){
         followAllRedirects: true,
         agent: false
 
-      } // end options
+      } // end options 
       
       const req = https.request(options, async res => {
         console.log(`statusCode: ${res.statusCode}`)
-        console.log('headers:', res.headers);
       
         res.on('data', d => {
-            //return JSON.parse(d.toString())
             fetched = d.toString();
             fs.writeFileSync(`data/${id}.json`, JSON.stringify(d.toString()))
         })
@@ -28,7 +26,7 @@ const reqData = async function(id){
       }) // end request callback
 
       req.on('error', error => {
-        console.log("current ID is: ", options.path)
+        console.log("Error found. Current ID is: ", options.path)
         console.error(error)
 
       })
@@ -36,40 +34,6 @@ const reqData = async function(id){
       req.end()
 }
 
-function httpsRequest(params, postData) {
-  return new Promise(function(resolve, reject) {
-      var req = https.request(params, function(res) {
-          // reject on bad status
-          if (res.statusCode < 200 || res.statusCode >= 300) {
-              return reject(new Error('statusCode=' + res.statusCode));
-          }
-          // cumulate data
-          var body = [];
-          res.on('data', function(chunk) {
-              body.push(chunk);
-          });
-          // resolve on end
-          res.on('end', function() {
-              try {
-                  body = JSON.parse(Buffer.concat(body).toString());
-              } catch(e) {
-                  reject(e);
-              }
-              resolve(body);
-          });
-      });
-      // reject on request error
-      req.on('error', function(err) {
-          // This is not a "Second reject", just a different sort of failure
-          reject(err);
-      });
-      if (postData) {
-          req.write(postData);
-      }
-      // IMPORTANT
-      req.end();
-  });
-}
 
 
-module.exports =  { reqData, httpsRequest }
+module.exports =  { reqData }
