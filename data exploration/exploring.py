c9bb4bc01c15a2cc3ba0df37f2e80fa1411c8804
nathanabram <nathan@nathanabram.com>
2022-01-28 11:45:23 -0500

    cleaning

--- data exploration/exploring.py
+++ data exploration/exploring.py
@@ -2,7 +2,7 @@ import json
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("data.csv")
+df = pd.read_csv("./tables/data.csv")
 
 
 publicationCounts = df["publication"].value_counts(False, True, False)
